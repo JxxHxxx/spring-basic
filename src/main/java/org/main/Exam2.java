d354770d1ac0d5fb6092094079a09b70603827d3
JxxHxxx <leesin5498@naver.com>
2022-12-23 22:34:03 +0900

    TEST #2 V2 버전으로 버스 테스트

--- src/main/java/org/main/Exam2.java
+++ src/main/java/org/main/Exam2.java
@@ -1,10 +1,51 @@
 package org.main;
 
 import org.domain.v2.Member;
+import org.domain.v2.bus.Bus;
 import org.domain.v2.taxi.Taxi;
+
+import static org.domain.v2.bus.BusStatus.REST;
+import static org.domain.v2.bus.BusStatus.RUNNING;
+
 
 public class Exam2 {
     public static void main(String[] args) {
+        Bus busA = new Bus();
+        Bus busB = new Bus();
+        System.out.println("bus1 객체 id = " + busA.getId());
+        System.out.println("bus2 객체 id = " + busB.getId());
+
+        busA.drive(2);
+        System.out.println("탑승 승객 수 = " + busA.getPassenger());
+        System.out.println("잔여 승객 수 = " + (Bus.maxPassenger - busA.getPassenger()));
+        System.out.println("요금 확인 = " + busA.getPassenger() * busA.getBasicCharge());
+
+        busA.changeFuel(-50);
+        System.out.println("주유량 = "  + busA.getFuel());
+
+        busA.setStatus(REST);
+        busA.changeFuel(10);
+        System.out.println("상태 : " + busA.getStatus());
+        System.out.println("주유량 : " + busA.getFuel());
+
+        busA.setStatus(RUNNING);
+        busA.drive(45);
+
+        Integer tmp1 = busA.getPassenger();
+        busA.drive(5);
+        Integer tmp2 = busA.getPassenger();
+
+        busA.drive(0);
+        System.out.println("탑승 승객 수 = " + (tmp2 - tmp1));
+        System.out.println("잔여 승객 수 = " + (Bus.maxPassenger - busA.getPassenger()));
+        System.out.println("요금 확인 = " + (tmp2 - tmp1) * busA.getBasicCharge());
+
+        busA.changeFuel(-55);
+        System.out.println("주유량 : " + busA.getFuel());
+        busA.drive(0);
+        System.out.println("상태 : " + busA.getStatus());
+        System.out.println("========================================================구분선=========================================================");
+        System.out.println("========================================================구분선=========================================================");
         Taxi taxiA = new Taxi();
         Taxi taxiB = new Taxi();
 

7fd6424371f9ecc34b9ded4cda33e49c4a78701b
JxxHxxx <leesin5498@naver.com>
2022-12-23 21:23:29 +0900

    FEAT #2 Taxi 객체 필요한 필드, 기본 생성자 추가

--- src/main/java/org/domain/v2/taxi/Taxi.java
+++ src/main/java/org/domain/v2/taxi/Taxi.java
@@ -1,4 +1,31 @@
 package org.domain.v2.taxi;
 
-public class Taxi {
+import org.domain.v2.Drivable;
+import org.domain.v2.Vehicle;
+import org.domain.v2.valid.CapacityValidator;
+import org.domain.v2.valid.Validator;
+
+import static org.domain.v2.taxi.TaxiStatus.NORMAL;
+import static org.domain.v2.taxi.TaxiStatus.RUNNING;
+
+public class Taxi extends Vehicle<TaxiStatus>{
+
+    private static Long sequence = 0L;
+    public static final Integer ratePerDistance = 1000;
+    public static final Integer maxPassenger = 4;
+    public static final Integer basicDistance = 1;
+
+
+    private static final Validator capacityValidator = CapacityValidator.getInstance();
+
+    private Integer distance;
+    private String destination;
+
+    public Taxi() {
+        this.id = sequence++;
+        this.basicCharge = 3000;
+        this.status = NORMAL;
+    }
+
+
 }

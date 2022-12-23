a799903a8d6b1e0068b64bcd495af056c1e8a0f9
JxxHxxx <leesin5498@naver.com>
2022-12-23 12:20:46 +0900

    FEAT #1 Vehicle 객체 Getter 생성

--- src/main/java/org/domain/Vehicle.java
+++ src/main/java/org/domain/Vehicle.java
@@ -12,4 +12,23 @@ public abstract class Vehicle {
         this.speed = 0;
         this.passenger = 0;
     }
+    public Integer getPassenger() {
+        return passenger;
+    }
+
+    public Integer getGasAmount() {
+        return gasAmount;
+    }
+
+    public Integer getSpeed() {
+        return speed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getCharge() {
+        return charge;
+    }
 }

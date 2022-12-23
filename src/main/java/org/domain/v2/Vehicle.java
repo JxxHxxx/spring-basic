43d576f822f17038afaefa41165e854378d20129
JxxHxxx <leesin5498@naver.com>
2022-12-23 17:45:06 +0900

    FEAT #2 Vehicle 필드 생성, 생성자 구현

--- src/main/java/org/domain/v2/Vehicle.java
+++ src/main/java/org/domain/v2/Vehicle.java
@@ -1,5 +1,15 @@
 package org.domain.v2;
 
 public abstract class Vehicle<T> implements Drivable, StateConverter{
+    String id;
+    Integer fuel;
+    Integer speed;
+    Integer maxPassenger;
+    Integer passenger;
+    T status;
 
+    public Vehicle() {
+        this.fuel = 100;
+        this.speed = 0;
+    }
 }

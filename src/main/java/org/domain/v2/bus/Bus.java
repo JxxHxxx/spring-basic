9dc5ec3c3a49a6b9d9b12128cdf2511070277a9b
JxxHxxx <leesin5498@naver.com>
2022-12-23 21:41:02 +0900

    FEAT #2 버스 객체 필드, 생성자 추가

--- src/main/java/org/domain/v2/bus/Bus.java
+++ src/main/java/org/domain/v2/bus/Bus.java
@@ -3,7 +3,20 @@ package org.domain.v2.bus;
 import org.domain.v2.Drivable;
 import org.domain.v2.Vehicle;
 
+import static org.domain.v2.bus.BusStatus.RUNNING;
+
 public class Bus extends Vehicle<BusStatus> implements Drivable<Integer> {
+
+    private static Long sequence = 0L;
+    private static final Integer maxPassenger = 30;
+
+    public Bus() {
+        super();
+        this.id = sequence++;
+        this.status = RUNNING;
+        this.basicCharge = 1000;
+
+    }
 
     @Override
     public void drive(Integer passenger) {

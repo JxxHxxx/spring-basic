28218d391c5e5ebc918060176d1e507a756c708a
JxxHxxx <leesin5498@naver.com>
2022-12-23 21:26:46 +0900

    FEAT #2 Taxi 객체 Drivable 인터페이스 구현
    drive 메소드 구현

--- src/main/java/org/domain/v2/taxi/Taxi.java
+++ src/main/java/org/domain/v2/taxi/Taxi.java
@@ -8,7 +8,7 @@ import org.domain.v2.valid.Validator;
 import static org.domain.v2.taxi.TaxiStatus.NORMAL;
 import static org.domain.v2.taxi.TaxiStatus.RUNNING;
 
-public class Taxi extends Vehicle<TaxiStatus>{
+public class Taxi extends Vehicle<TaxiStatus> implements Drivable<Member>{
 
     private static Long sequence = 0L;
     public static final Integer ratePerDistance = 1000;
@@ -26,6 +26,37 @@ public class Taxi extends Vehicle<TaxiStatus>{
         this.basicCharge = 3000;
         this.status = NORMAL;
     }
+
+    @Override
+    public void drive(Member member) {
+        if (!nowStatus(NORMAL)) {
+            System.out.println("MESSAGE : [현재 택시의 상태 : " + this.status + ". 탑승이 불가능합니다.]");
+            return;
+        }
+
+        this.passenger = member.getNumber();
+
+        if (capacityValidator.execute(this, 4)) {
+            System.out.println("MESSAGE : [수용 인원이 초과되어 승객을 태울 수 없습니다.]");
+            this.passenger = 0;
+            return;
+        }
+
+        this.destination = member.getDestination();
+        this.distance = member.getDistance();
+        this.status = RUNNING;
+    }
+
+    private boolean nowStatus(TaxiStatus status) {
+        return this.status.equals(status);
+    }
+
+
+    @Override
+    public void run() {
+
+    }
+
 
 
 }

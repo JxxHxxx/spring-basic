95772dc9cd97c1e9cb51cecac3b6dfa68661798d
JxxHxxx <leesin5498@naver.com>
2022-12-23 21:24:17 +0900

    FEAT #2 Member 객체 필요한 필드, 기본 생성자 추가

---
+++ src/main/java/org/domain/v2/taxi/Member.java
@@ -0,0 +1,30 @@
+package org.domain.v2.taxi;
+
+public class Member {
+    private Long id;
+    private Integer number;
+    private String destination;
+    private Integer distance;
+
+    public Member(Integer number, String destination, Integer distance) {
+        this.number = number;
+        this.destination = destination;
+        this.distance = distance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+}

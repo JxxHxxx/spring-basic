417de10e4a8379b97da0bb8d2c663cf9d3dfb614
JxxHxxx <leesin5498@naver.com>
2022-12-23 12:16:28 +0900

    FEAT #1 버스 객체 필드 및 생성자 구현
    id 필드 - 고유값 지정
    stauts 필드 - RUNNING
    charge 필드 - 1000

---
+++ src/main/java/org/domain/Bus.java
@@ -0,0 +1,19 @@
+package org.domain;
+
+import static org.domain.BusStatus.RUNNING;
+
+public class Bus extends Vehicle{
+
+    private static Long sequence = 0L;
+    private static final Integer maxPassenger = 30;
+    private BusStatus status;
+
+    public Bus() {
+        super();
+        this.id = sequence++;
+        this.status = RUNNING;
+        this.charge = 1000;
+    }
+
+
+}

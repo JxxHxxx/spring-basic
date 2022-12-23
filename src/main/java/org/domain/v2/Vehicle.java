1781241622205ce37ef44a15734057aac66c84c8
JxxHxxx <leesin5498@naver.com>
2022-12-23 21:21:09 +0900

    FEAT #2 계산 기능을 위한 필드 추가, 생성자 변경

--- src/main/java/org/domain/v2/Vehicle.java
+++ src/main/java/org/domain/v2/Vehicle.java
@@ -6,10 +6,13 @@ public abstract class Vehicle<Status> {
     protected Integer speed;
     protected Integer passenger;
     protected Integer basicCharge;
+    protected Integer wallet;
     protected Status status;
 
     public Vehicle() {
         this.fuel = 100;
         this.speed = 0;
+        this.passenger = 0;
+        this.wallet = 0;
     }
 }
